0cdbf255c68f0c55c8fb3c8f493cc2829a0ed640
jaeskim <jaeskim@student.42seoul.kr>
2021-06-07 05:41:49 +0900

    :sparkles: add post

--- intranet/models/post.py
+++ intranet/models/post.py
@@ -1,19 +1,16 @@
 from django.db import models
 from django.conf import settings
 
+
 class PostModel(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128, null=False)
-    # TODO: User model에 대한 검증 후 다시 확인 하기!
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='id')
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(null=False)
     creationDate = models.DateTimeField(auto_now_add=True, null=False)
     updateDate = models.DateTimeField(auto_now=True, null=False)
-    # TODO: comments 객체와 One To Manny로 연결 할 것!
-    # comments = ????
 
-    # add comments
     class Meta:
         ordering = ('creationDate',)
 
@@ -21,4 +18,4 @@ class PostModel(models.Model):
         return self.title
 
     def get_comments(self):
-        return self.comments.filter(parent=None).filter(active=True) # parent가 없고 걸러지지 않은 댓글
+        return self.comments.filter(parent=None).filter(active=True)
--- intranet/views/post.py
+++ intranet/views/post.py
@@ -1,24 +1,45 @@
+from typing import Any, Dict
+from django import http
+from django.contrib import messages
 from django.http import request
-from django.http.response import HttpResponse
-from django.urls.base import reverse_lazy
+from django.http.response import Http404, HttpResponse
+from django.urls.base import reverse_lazy, reverse
 from django.views.generic import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.detail import DetailView
 from intranet.models import PostModel
 from intranet.forms import PostForm
 
 
+class PostUserCheckMixin:
+    def dispatch(self, request: http.HttpRequest, post_id,  *args: Any, **kwargs: Any) -> HttpResponse:
+        self.post_id = post_id
+        try:
+            post: PostModel = PostModel.objects.get(id=post_id)
+        except PostModel.DoesNotExist:
+            raise Http404()
+        if post.author != request.user and not request.user.is_staff:
+            messages.error(request, "permission denied")
+            return redirect('intranet:main')
+        self.instance = post
+        return super().dispatch(request, post_id, *args, **kwargs)
+
+
 class PostView(LoginRequiredMixin, FormView):
     template_name = 'intranet/pages/post/new.html'
     form_class = PostForm
-    success_url = reverse_lazy('intranet:main')
     login_url = reverse_lazy('intranet:login')
+
+    def get_success_url(self) -> str:
+        return reverse('intranet:post-detail', args=[self.instance.id])
 
     def form_valid(self, form: PostForm) -> HttpResponse:
         post: PostModel = form.save(commit=False)
         post.author = self.request.user
         post.save()
+        self.instance = post
+        messages.success(self.request, 'Create post success!')
         return super().form_valid(form)
 
     def form_invalid(self, form: PostForm) -> HttpResponse:
@@ -30,3 +51,28 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     login_url = reverse_lazy('intranet:login')
     model = PostModel
     pk_url_kwarg = 'post_id'
+
+
+class PostEditView(LoginRequiredMixin, PostUserCheckMixin, FormView):
+    template_name = 'intranet/pages/post/edit.html'
+    form_class = PostForm
+    login_url = reverse_lazy('intranet:login')
+
+    def get_success_url(self) -> str:
+        return reverse('intranet:post-detail', kwargs={'post_id': self.post_id})
+
+    def get_initial(self) -> Dict[str, Any]:
+        post: PostModel = self.instance
+        initial = super().get_initial()
+        initial['title'] = post.title
+        initial['content'] = post.content
+        return initial
+
+    def form_valid(self, form: PostForm) -> HttpResponse:
+        self.instance.title = form.cleaned_data['title']
+        self.instance.content = form.cleaned_data['content']
+        self.instance.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form: PostForm) -> HttpResponse:
+        return super().form_invalid(form)
